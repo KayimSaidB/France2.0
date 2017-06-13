79f5eaab6299d34dc2ab7ec01298c68e95fff4cf
KayimSaidB <kayimsaid@hotmail.fr>
2017-06-13 13:26:17 +0200

    meilleure gestion des elections
    presidentielles fonctionnel

--- Francemapping.sol
+++ Francemapping.sol
@@ -1,36 +1,69 @@
 pragma solidity ^0.4.11;
 
 contract SystemeFrance{
-    /// We start by defining citizens by an adress
+    /// We start by defining citizens by an adress 
     // and what we call a role by default a citizen is a normal citizen
     enum Roles{Simple_citoyen,Depute,Senateur,President,Conseiller_constit}
-      struct Voter {
-        bool votedP;bool voteD;bool votedS;
-        Roles RoleDuCitoyen;
-
-      }
-
-    mapping(address => Voter) citoyens;
-
-event voteCast(address citoyen, Roles place, bool voted);
-event voteCast2(address chien);
+     
+      
+   struct election 
+	{
+		bool isElecting;
+		uint nbCandidates;
+		mapping (address => bool) hasVoted;
+		mapping (uint => address) candidateList;
+		mapping (address => uint256) electionResults;
+	} 
+mapping(address => Roles) citoyens;
+event rolecast(address citoyen, Roles sonrole);
 function affichage_role(address[]citoyen_){
-
         for (uint256 i;i<citoyen_.length;i++){
-            voteCast(citoyen_[i],citoyens[citoyen_[i]].RoleDuCitoyen,citoyens[citoyen_[i]].votedP);
-
+            Roles petit=citoyens[citoyen_[i]];
+            rolecast(citoyen_[i],petit);
+            
 }
 }
 
-/// du point de vue du votant cette fonction
-mapping(address =>uint256) listecandidat;
-function vote_for_president(address candidat){
-      if (citoyens[msg.sender].votedP==false){
-          listecandidat[candidat]++;
-          citoyens[msg.sender].votedP=true;
-          voteCast2(msg.sender);
-      }
+
+election presidentielles;
+/// starts presidential election
+function start_pres(address[]candidats){
+        presidentielles.isElecting=true;
+        for(uint i=0;i<candidats.length;i++) presidentielles.candidateList[i]=candidats[i];
+        presidentielles.nbCandidates=candidats.length;
+    
+}
+
+
+
+/// from the voter  
+function vote_for_president(address candidat) returns (bool){
+      if ((presidentielles.hasVoted[msg.sender]==false) && (presidentielles.isElecting==true)){
+          presidentielles.electionResults[candidat]++;
+          presidentielles.hasVoted[msg.sender]=true;
+          return true;
+         }
+    return false;
+}
+// ends presidential election and set the new president
+function get_the_new_president(){
+         presidentielles.isElecting=false;
+
+        address winner=presidentielles.candidateList[0];
+
+        for (uint i=1;i<presidentielles.nbCandidates;i++){
+            if (presidentielles.electionResults[presidentielles.candidateList[i]]>presidentielles.electionResults[winner]){
+                winner=presidentielles.candidateList[i];
+                
+            }
+            
+        }
+    citoyens[winner]=Roles.President;
+    
+    
+}
+
+
 
 }
 
-}
