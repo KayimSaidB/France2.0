7483c2205060e015ed912b45f1187248997e3ef7
KayimSaidB <kayimsaid@hotmail.fr>
2017-06-15 17:01:17 +0200

    Update senat.sol

--- senat.sol
+++ senat.sol
@@ -16,17 +16,19 @@ contract SystemeFranceSenat{
 		bool isElecting;
 		uint nbCandidates;
 		mapping (address => bool) hasVoted;
-		mapping (uint => address[]) candidateList;
-		mapping (address => uint256) electionResults;
+		mapping (uint => address[3]) candidateList;
+    
+		mapping (uint => uint256) electionResults;
 	} 
 mapping(address => carac_citoyen) citoyens;
 event rolecast(address citoyen, Roles sonrole);
 /// vote for the Senat
-/*function register(uint codepostal,uint numero_district){
+function register(uint codepostal,uint numero_district,uint numberdepartement){
     citoyens[msg.sender].codepostal=codepostal;
     citoyens[msg.sender].numerodistrict=numero_district;
+    citoyens[msg.sender].numberdepartement=numberdepartement;
 }
-*/
+
 function affichage_role(address[]citoyen_){
         for (uint256 i;i<citoyen_.length;i++){
             Roles petit=citoyens[citoyen_[i]].RoleDuCitoyen;
@@ -34,23 +36,33 @@ function affichage_role(address[]citoyen_){
             
 }
 }
-election Senat;
 // each county has a list of Senators 
 // so we create a mapping which links the number of Senators needed 
 /// and the number of a county
 mapping (uint =>uint) departement;
-mapping (uint=>election) liste_senat_pre_tour; //  number of a county linked to each election case we have to elect a senator in two turn
+mapping (uint=>election) liste_senat_pre_tour;
+mapping (uint=>election) liste_senat_sec_tour;
+//number of a county linked to each election case we have to elect a senator in two turn
+// if the number of adresses is under 3 we need to fill every list with 0x0
+//or modify this part
 function start_a_senat_pre_tour(address[3][]candidats,uint number_departement){
         liste_senat_pre_tour[number_departement].isElecting=true;
         for(uint i=0;i<candidats.length;i++) liste_senat_pre_tour[number_departement].candidateList[i]=candidats[i];
         liste_senat_pre_tour[number_departement].nbCandidates=candidats.length;
     }
 
-/// when you vote for an election you chose an array of addresses
-/// you can vote 
-function vote_for_a_senat_pre_tour(address [3]candidat,uint number_departement) returns (bool){
+/// when you vote for an election you chose 
+/// we get the results with the address of the first candidate
+event get_name(address[3]a_list, uint itsindice);
+function show_me_the_candidates(uint number_departement){
+    for (uint i=0;i<liste_senat_pre_tour[number_departement].nbCandidates;i++)get_name(liste_senat_pre_tour[number_departement].candidateList[i],i);
+}
+
+
+
+function vote_for_a_senat_pre_tour(uint indice_liste,uint number_departement) returns (bool){
 if ((liste_senat_pre_tour[number_departement].hasVoted[msg.sender]==false) && (liste_senat_pre_tour[number_departement].isElecting==true) && (number_departement==citoyens[msg.sender].numberdepartement)){
-          liste_senat_pre_tour[number_departement].electionResults[candidat[0]]++;
+          liste_senat_pre_tour[number_departement].electionResults[indice_liste]++;
           liste_senat_pre_tour[number_departement].hasVoted[msg.sender]=true;
           return true;
          }
@@ -58,68 +70,108 @@ if ((liste_senat_pre_tour[number_departement].hasVoted[msg.sender]==false) && (l
     
     
 }
-/*
-function get_the_two_or_the_one_depute(uint number_district)returns(address[2]){
-         liste_legislatives_pre_tour[number_district].isElecting=false;
-        address winner=liste_legislatives_pre_tour[number_district].candidateList[0];
-        address second=liste_legislatives_pre_tour[number_district].candidateList[1];
-       
+
+function get_the_two_or_the_one_list(uint number_departement)returns(address[3][2]){
+         liste_senat_pre_tour[number_departement].isElecting=false;
+        address[3] winner=liste_senat_pre_tour[number_departement].candidateList[0];
+        address [3]second=liste_senat_pre_tour[number_departement].candidateList[1];
+        uint indicewinner=0;
+        uint indicesecond=1;
         uint256 compteur=0;
-       address[2] memory vainqueur;
-        for (uint i=0;i<liste_legislatives_pre_tour[number_district].nbCandidates;i++){
-            if (liste_legislatives_pre_tour[number_district].electionResults[liste_legislatives_pre_tour[number_district].candidateList[i]]>liste_legislatives_pre_tour[number_district].electionResults[winner]){
+       address[3][2] memory vainqueur;
+       for (uint i=0;i<liste_senat_pre_tour[number_departement].nbCandidates;i++){
+            if (liste_senat_pre_tour[number_departement].electionResults[i]>liste_senat_pre_tour[number_departement].electionResults[indicewinner]){
                 second=winner;
-                winner=liste_legislatives_pre_tour[number_district].candidateList[i];
-                }
-            if (liste_legislatives_pre_tour[number_district].electionResults[liste_legislatives_pre_tour[number_district].candidateList[i]]>liste_legislatives_pre_tour[number_district].electionResults[second]){
-                second=liste_legislatives_pre_tour[number_district].candidateList[i];
-            
+                winner=liste_senat_pre_tour[number_departement].candidateList[i];
+                indicesecond=indicewinner;
+                indicewinner=i;
+                
             }
-           compteur+=liste_legislatives_pre_tour[number_district].electionResults[liste_legislatives_pre_tour[number_district].candidateList[i]]; 
+            if (liste_senat_pre_tour[number_departement].electionResults[i]>liste_senat_pre_tour[number_departement].electionResults[indicesecond]){
+                second=liste_senat_pre_tour[number_departement].candidateList[i];
+                indicesecond=i;
+            }
+           compteur+=liste_senat_pre_tour[number_departement].electionResults[i]; 
         }
         
- if (liste_legislatives_pre_tour[number_district].electionResults[winner]>compteur/2){ 
-     citoyens[winner].RoleDuCitoyen=Roles.Depute;
+ if (liste_senat_pre_tour[number_departement].electionResults[indicewinner]>compteur/2){ 
+     for (uint j=0;j<winner.length;j++)citoyens[winner[j]].RoleDuCitoyen=Roles.Senateur;
+     
+     // a modfier selon le nombre de senateur
     vainqueur[0]=winner;
     return vainqueur;
     
  }
 else { 
+    
     vainqueur[0]=winner;
     vainqueur[1]=second;
     return vainqueur;
 }
 }
-function start_a_legi_sec_tour(address[]candidats,uint number_district){
-        liste_legislatives_sec_tour[number_district].isElecting=true;
-        for(uint i=0;i<candidats.length;i++) liste_legislatives_sec_tour[number_district].candidateList[i]=candidats[i];
-        liste_legislatives_sec_tour[number_district].nbCandidates=candidats.length;
+
+function start_a_senat_sec_tour(address[3][]candidats,uint number_departement){
+        liste_senat_sec_tour[number_departement].isElecting=true;
+        for(uint i=0;i<candidats.length;i++) liste_senat_sec_tour[number_departement].candidateList[i]=candidats[i];
+        liste_senat_sec_tour[number_departement].nbCandidates=candidats.length;
     }
-function vote_for_secondtour_legi(address candidat,uint number_district) returns (bool){
-      if ((liste_legislatives_sec_tour[number_district].hasVoted[msg.sender]==false) && (liste_legislatives_sec_tour[number_district].isElecting==true) && (citoyens[msg.sender].numerodistrict==number_district)){
-          liste_legislatives_sec_tour[number_district].electionResults[candidat]++;
-          liste_legislatives_sec_tour[number_district].hasVoted[msg.sender]=true;
+
+function vote_for_a_senat_sec_tour(uint indice_liste,uint number_departement) returns (bool){
+if ((liste_senat_sec_tour[number_departement].hasVoted[msg.sender]==false) && (liste_senat_sec_tour[number_departement].isElecting==true) && (number_departement==citoyens[msg.sender].numberdepartement)){
+          liste_senat_sec_tour[number_departement].electionResults[indice_liste]++;
+          liste_senat_sec_tour[number_departement].hasVoted[msg.sender]=true;
           return true;
          }
-    return false;
+    return false;    
+    
+    
 }
 
-function get_depute_after_second_tour(uint number_district){
-    address winner;
-    liste_legislatives_sec_tour[number_district].isElecting=false;
-    if(liste_legislatives_sec_tour[number_district].electionResults[liste_legislatives_sec_tour[number_district].candidateList[0]]>liste_legislatives_sec_tour[number_district].electionResults[liste_legislatives_sec_tour[number_district].candidateList[1]]){
-    winner=liste_legislatives_sec_tour[number_district].candidateList[0];
+
+function get_senators_after_second_tour(uint number_departement){
+    address [3] memory winner;
+    liste_senat_sec_tour[number_departement].isElecting=false;
+    if(liste_senat_sec_tour[number_departement].electionResults[0]>liste_senat_sec_tour[number_departement].electionResults[1]){
+    winner=liste_senat_sec_tour[number_departement].candidateList[0];
     }
-    else winner=liste_legislatives_sec_tour[number_district].candidateList[1];
+    else winner=liste_senat_sec_tour[number_departement].candidateList[1];
     
-    
-citoyens[winner].RoleDuCitoyen=Roles.Depute;
+       for (uint j=0;j<winner.length;j++)citoyens[winner[j]].RoleDuCitoyen=Roles.Senateur;
 
     
 }
+
+   struct election_proportional 
+	{
+		bool isElecting;
+		uint nbCandidates;
+		mapping (address => bool) hasVoted;
+		mapping (uint => address[]) candidateList;
+    
+		mapping (uint => uint256) electionResults;
+	} 
+
+mapping (uint=>election_proportional) liste_senat_proportional;
+/// set to the maximal senators 
+
+function register_proportional(int nombre_depute,uint number_departement)
+{
+    //address [nombre_depute] memory candidate;
+    
+    address[] memory candidate= new address[nombre_depute];
+}
+function start_a_senat_proportional(address[12][]candidats,uint number_departement){
+        liste_senat_proportional[number_departement].isElecting=true;
+        for(uint i=0;i<candidats.length;i++) liste_senat_proportional[number_departement].candidateList[i]=candidats[i];
+        liste_senat_pre_tour[number_departement].nbCandidates=candidats.length;
+    }
+
+
+function get_senators_after_proportional(){}
+
 //////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////Election président Assemblée Nationale/////////////////
-
+/*
 /// the President of the National Assembly is elected with absolute majority
 /// if we cant reach absolute majority twice, we must use relative majority
 election presidentAN;
@@ -208,4 +260,5 @@ function get_Presdepute_normal2() returns (address){
    */
    
 }
+
 
