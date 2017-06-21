404b51c3ef37913efc180f3ae30930d3d16b0ccc
KayimSaidB <kayimsaid@hotmail.fr>
2017-06-21 17:51:59 +0200

    Update Francelocal.sol

--- Francelocal.sol
+++ Francelocal.sol
@@ -7,30 +7,24 @@ contract SystemeFranceLocal is gestionStructure{
     // and what we call a role by default a citizen is a normal citizen
 
     
-   struct election 
-	{
-		bool isElecting;
-		uint nbCandidates;
-		mapping (address => bool) hasVoted;
-		mapping (uint => address) candidateList;
-		mapping (address => uint256) electionResults;
-	} 
+ 
 	///This code is designed for local election but can be adapted to any regional-like
 event rolecast(address citoyen, Roles sonrole);
 ///enables a citizen to register himself with its postal code and its number of district
 /// Maybe a register with all the codepostal linked to the number of district could be useful
 ///in order to have a more user friendly interface 
-function register(uint codepostal,uint numero_district, uint numberdepartement) returns(bool){
+function register(uint codepostal,uint numero_district, uint numberdepartement,bytes32 nom) returns(bool){
     if((citoyens[msg.sender].codepostal ==0) &&(citoyens[msg.sender].numerodistrict==0)){
     citoyens[msg.sender].codepostal=codepostal;
     citoyens[msg.sender].numerodistrict=numero_district;
     citoyens[msg.sender].numberdepartement=numberdepartement;
+    citoyens[msg.sender].name=nom;
         return true;
     }
     return false;
 }
   
-function affichage_role(address[]citoyen_){
+function show_role(address[]citoyen_){
         for (uint256 i;i<citoyen_.length;i++){
             Roles petit=citoyens[citoyen_[i]].RoleDuCitoyen;
             rolecast(citoyen_[i],petit);
@@ -40,6 +34,8 @@ function affichage_role(address[]citoyen_){
 /// we create a mapping with election and the postal code connected
 mapping(uint=>election ) liste_locales_pre_tour;
 mapping(uint=>election ) liste_locales_sec_tour;
+/// a register with all local elected
+mapping(uint=>address) register_lcl;
 
 // each election is represented by its postal code 
 /// for one city
@@ -61,7 +57,7 @@ if ((liste_locales_pre_tour[postal_code].hasVoted[msg.sender]==false) && (liste_
     
     
 }
-function get_the_two_or_the_one_conseiller(uint postal_code)returns(address[2]){
+function set_the_two_or_the_one_conseiller(uint postal_code)returns(address[2]){
          liste_locales_pre_tour[postal_code].isElecting=false;
         address winner=liste_locales_pre_tour[postal_code].candidateList[0];
         address second=liste_locales_pre_tour[postal_code].candidateList[1];
@@ -82,6 +78,8 @@ function get_the_two_or_the_one_conseiller(uint postal_code)returns(address[2]){
         
  if (liste_locales_pre_tour[postal_code].electionResults[winner]>compteur/2){ 
      citoyens[winner].RoleDuCitoyen=Roles.Conseiller_local;
+      register_lcl[postal_code]=winner;
+
     vainqueur[0]=winner;
     return vainqueur;
     
@@ -106,7 +104,7 @@ function vote_for_secondtour_lcl(address candidat,uint postal_code) returns (boo
     return false;
 }
 
-function get_conseiller_lcl_after_second_tour(uint postal_code){
+function set_conseiller_lcl_after_second_tour(uint postal_code){
     address winner;
     liste_locales_sec_tour[postal_code].isElecting=false;
     if(liste_locales_sec_tour[postal_code].electionResults[liste_locales_sec_tour[postal_code].candidateList[0]]>liste_locales_sec_tour[postal_code].electionResults[liste_locales_sec_tour[postal_code].candidateList[1]]){
@@ -116,7 +114,11 @@ function get_conseiller_lcl_after_second_tour(uint postal_code){
     
     
 citoyens[winner].RoleDuCitoyen=Roles.Conseiller_local;
+register_lcl[postal_code]=winner;
 
     
+}
+function get_my_local( uint postal_code) returns (address){
+    return register_lcl[postal_code];
 }
 }
