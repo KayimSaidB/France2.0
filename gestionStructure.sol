f1fd8de3171f91ed610187e508a355c78799c8ad
KayimSaidB <kayimsaid@hotmail.fr>
2017-06-21 17:53:26 +0200

    Update gestionStructure.sol

--- gestionStructure.sol
+++ gestionStructure.sol
@@ -3,6 +3,7 @@ contract gestionStructure{
     
      enum Roles{Simple_citoyen,PresidentR,Conseiller_local,Depute,PresidentAN,Senateur,PresidentS,Conseiller_constit}
     struct carac_citoyen{
+         bytes32 name;
          uint codepostal;
          Roles RoleDuCitoyen;
          uint numerodistrict;
@@ -12,7 +13,14 @@ contract gestionStructure{
      mapping(address => carac_citoyen) citoyens; /// the regisiter of citizen, they have an address connected to citizen characteristics
 
     
-    
+      struct election 
+	{
+		bool isElecting;
+		uint nbCandidates;
+		mapping (address => bool) hasVoted;
+		mapping (uint => address) candidateList;
+		mapping (address => uint256) electionResults;
+	} 
     
     
 }
